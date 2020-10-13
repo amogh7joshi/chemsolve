45de72fe5ae53135ae0f32cf122cf5d3bd27dfff
amogh7joshi <joshi.amoghn@gmail.com>
2020-10-13 15:20:08 -0400

    Updated travis and added new constants

--- chemsolve/__init__.py
+++ chemsolve/__init__.py
@@ -5,7 +5,7 @@ warnings.filterwarnings("ignore")
 from .element import Element, SpecialElement
 from .compound import Compound, FormulaCompound
 from .compound import CarbonDioxide, Water
-from .reaction import Reaction ,CombustionTrain
+from .reaction import Reaction, CombustionTrain
 
 from .electrochemistry.molar import molarity
 
--- chemsolve/utils/constants.py
+++ chemsolve/utils/constants.py
@@ -2,6 +2,23 @@
 Any prevalent constants in chemistry.
 '''
 
+# BASIC
+
+# Avogadro's Constant
 AVOGADRO = 6.022e23
-ELEMENTARY_CHARGE = 1.60e-19
-ATOMIC_MASS_UNIT = 1.66e-27
+# Elementary Charge
+C = 1.60e-19
+# Atomic Mass Unit
+M = 1.66e-27
+
+# ELECTROCHEMISTRY
+
+# Faraday's Constant
+F = 96485.33289
+
+# GASES
+
+# Molar Gas
+R = 8.3144598
+# Boltzmann
+K = 1.380649e-23
