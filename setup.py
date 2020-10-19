b6c54054e107af3041de3442e3b94d6a0a0874bd
amogh7joshi <joshi.amoghn@gmail.com>
2020-10-19 15:28:28 -0400

    If this doesn't work I give up

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ setup(
       "Programming Language :: Python :: 3",
       "License :: OSI Approved :: MIT License",
       "Operating System :: OS Independent"
-   ]
+   ],
+   include_package_data = True
 )
