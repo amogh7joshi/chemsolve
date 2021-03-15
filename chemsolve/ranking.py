2568b6644ff816a7380cb8a6e70bb733c6071ed7
amogh7joshi <joshi.amoghn@gmail.com>
2021-03-15 19:02:26 -0400

    Added methods for ranking first ionization energy and electron affinity

--- chemsolve/ranking.py
+++ chemsolve/ranking.py
@@ -6,6 +6,11 @@ from chemsolve.utils.errors import InvalidElementError
 def rank_electronegativity(*elements):
    """Ranks provided elements by their electronegativity.
 
+   Examples
+   --------
+   >>> print(rank_electronegativity(
+   ...   Element("Ca"), Element("K"), Element("Br"), Element("O"))))
+
    Parameters
    ----------
    elements: chemsolve.Element
@@ -34,6 +39,11 @@ def rank_electronegativity(*elements):
 def rank_atomic_radius(*elements):
    """Ranks provided elements by their atomic radii.
 
+   Examples
+   --------
+   >>> print(rank_atomic_radius(
+   ...   Element("Ca"), Element("K"), Element("Br"), Element("O"))))
+
    Parameters
    ----------
    elements: chemsolve.Element
@@ -59,3 +69,68 @@ def rank_atomic_radius(*elements):
    # Return the sorted list.
    return sorted_elements
 
+def rank_first_ionization_energy(*elements):
+   """Ranks provided elements by their first ionization energy.
+
+   Examples
+   --------
+   >>> print(rank_first_ionization_energy(
+   ...   Element("Ca"), Element("K"), Element("Br"), Element("O")))
+
+   Parameters
+   ----------
+   elements: chemsolve.Element
+      A list of `chemsolve.Element` objects.
+
+   Returns
+   -------
+   The elements ranked from least to greatest first ionization energy.
+   """
+   # First, validate the provided elements.
+   for element in elements:
+      if not isinstance(element, Element):
+         raise InvalidElementError(element, property_type = "type")
+
+   # Then, get the first ionization energy of each element.
+   ionization_energies = []
+   for element in elements:
+      ionization_energies.append(element.ionization)
+
+   # Sort the atomic radii.
+   _, sorted_elements = zip(*sorted(zip(ionization_energies, elements)))
+
+   # Return the sorted list.
+   return sorted_elements
+
+def rank_electron_affinity(*elements):
+   """Ranks provided elements by their electron_affinity.
+
+   Examples
+   --------
+   >>> print(rank_electron_affinity(
+   ...   Element("Ca"), Element("K"), Element("Br"), Element("O")))
+
+   Parameters
+   ----------
+   elements: chemsolve.Element
+      A list of `chemsolve.Element` objects.
+
+   Returns
+   -------
+   The elements ranked from least to greatest electron affinity.
+   """
+   # First, validate the provided elements.
+   for element in elements:
+      if not isinstance(element, Element):
+         raise InvalidElementError(element, property_type = "type")
+
+   # Then, get the first ionization energy of each element.
+   electron_affinities = []
+   for element in elements:
+      electron_affinities.append(element.electron_affinity)
+
+   # Sort the atomic radii.
+   _, sorted_elements = zip(*sorted(zip(electron_affinities, elements)))
+
+   # Return the sorted list.
+   return sorted_elements
