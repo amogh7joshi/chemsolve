627b88720cb9e9fa585ef157ace6e3bce5e3cf50
amogh7joshi <joshi.amoghn@gmail.com>
2021-03-15 19:53:54 -0400

    Rework Element, add complete docstrings, deprecate useless methods

--- chemsolve/element.py
+++ chemsolve/element.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding = utf-8 -*-
+from __future__ import division
+
 import operator
 
 from chemsolve.utils.periodictable import PeriodicTable
@@ -7,11 +11,41 @@ from chemsolve.utils.constants import *
 __all__ = ['Element', 'SpecialElement']
 
 class Element(object):
-   """
-   A class which contains an element.
+   """The core element object class.
 
-   Has little functions of its own aside from elemental attributes such as number, mass, and the ion it forms.
-   A framework for extended element classes.
+   This class contains a single element and its attributes are all
+   different properties of the element, including the simple ones like
+   mass and atomic number, but also electron configuration (both full and
+   noble gas abbreviation), atomic radius, electronegativity, ionization,
+   and electron affinity. These can all be accessed as attributes.
+
+   The element can be instantiated traditionally, from its symbol, but can
+   also be created from molar mass or noble gas/complete electron configuration.
+
+   Furthermore, with provided amounts of grams, moles, or molecules, the
+   element class can automatically calculate the second and third unknown
+   quantities, such as molecules and moles from grams.
+
+   Examples
+   --------
+   Create a Barium element.
+
+   >>> barium = Element('Ba')
+   >>> # Access the element's attributes.
+   >>> print(barium.mass)
+   >>> print(barium.electronegativity)
+
+   Create a Calcium element and calculate the number of moles.
+
+   >>> calcium = Element('Ca', grams = 2.0)
+   >>> # Get the number of molecules and moles.
+   >>> print(calcium.mole_amount)
+   >>> print(calcium.molecules)
+
+   Parameters
+   ----------
+   element_symbol: str
+      The element symbol representing the element you want to initialize.
    """
    def __init__(self, element_symbol, **kwargs):
       # Initialize class properties from PeriodicTable object.
@@ -29,56 +63,33 @@ class Element(object):
       self.electron_configuration = self._properties['ElectronConfiguration']
       self.full_electron_configuration = self._get_full_electron_configuration()
 
-      # Miscellaneous Properties
+      # Miscellaneous Properties.
       self.radius = self._properties['AtomicRadius']
       self.electronegativity = self._properties['Electronegativity']
       self.ionization = self._properties['IonizationEnergy']
       self.electron_affinity = self._properties['ElectronAffinity']
 
-      # The class can calculate quantities of moles and grams, depending on the specific keywords 'moles' and 'grams'.
+      # Class Value Calculations.
       if "moles" in kwargs:
          self.mole_amount = kwargs["moles"]
-         self.gram_amount = round(operator.mul(self.mole_amount, self.mass), 4)
-         self.molecules = round(operator.mul(self.mole_amount, AVOGADRO), 4)
+         self.gram_amount = round(
+            operator.mul(self.mole_amount, self.mass), 4)
+         self.molecules = round(
+            operator.mul(self.mole_amount, AVOGADRO), 4)
 
       if "grams" in kwargs:
          self.gram_amount = kwargs["grams"]
-         self.mole_amount = round(operator.__truediv__(self.gram_amount, self.mass), 4)
-         self.molecules = round(operator.mul(self.mole_amount, AVOGADRO), 4)
+         self.mole_amount = round(
+            operator.__truediv__(self.gram_amount, self.mass), 4)
+         self.molecules = round(
+            operator.mul(self.mole_amount, AVOGADRO), 4)
 
       if "molecules" in kwargs:
          self.molecules = kwargs["molecules"]
-         self.mole_amount = round(operator.__truediv__(self.molecules, AVOGADRO), 4)
-         self.gram_amount = round(operator.mul(self.mass, self.mole_amount))
-
-      if all(x in ["moles", "grams", "kwargs"] for x in [kwargs]):
-         raise Exception("You cannot provide multiple quantities of the element at a single time.")
-
-   def __str__(self):
-      return str(self.element_name.title())
-
-   def __repr__(self):
-      return str(self.element_symbol.title())
-
-   def __getattr__(self, item):
-      if item == "percent_of": # Special feature, only available in the SpecialElement class.
-         raise AttributeError("If you want to use percentages, you must use the SpecialElement class.")
-
-   def __call__(self, **kwargs):
-      if "moles" in kwargs:
-         self.mole_amount = kwargs["moles"]
-         self.gram_amount = round(operator.mul(self.mole_amount, self.mass), 4)
-         self.molecules = round(operator.mul(self.mole_amount, AVOGADRO), 4)
-
-      if "grams" in kwargs:
-         self.gram_amount = kwargs["grams"]
-         self.mole_amount = round(operator.__truediv__(self.gram_amount, self.mass), 4)
-         self.molecules = round(operator.mul(self.mole_amount, AVOGADRO), 4)
-
-      if "molecules" in kwargs:
-         self.molecules = kwargs["molecules"]
-         self.mole_amount = round(operator.__truediv__(self.molecules, AVOGADRO), 4)
-         self.gram_amount = round(operator.mul(self.mass, self.mole_amount))
+         self.mole_amount = round(
+            operator.__truediv__(self.molecules, AVOGADRO), 4)
+         self.gram_amount = round(
+            operator.mul(self.mass, self.mole_amount))
 
       if "percent" in kwargs: # Primarily if you are setting up elements for Compound.from_formula.
          if float(kwargs["percent"]) >= 1:
@@ -88,21 +99,109 @@ class Element(object):
          self.gram_amount = False
 
       if all(x in ["moles", "grams", "kwargs"] for x in [kwargs]):
-         raise ValueError("You cannot provide multiple quantities of the element at a single time.")
+         raise Exception("You cannot provide multiple different quantities "
+                         "of the element at a single time.")
+
+   def __str__(self):
+      # For __str__, return the entire name of the element.
+      return str(self.element_name.title())
+
+   def __repr__(self):
+      # For __repr__, we only need the element symbol, since that will be the
+      # only thing used in internal methods (the intention of __repr__).
+      return str(self.element_symbol.title())
+
+   def __call__(self, **kwargs):
+      # Update the class calculation quantities for more calculations.
+      if "moles" in kwargs:
+         self.mole_amount = kwargs["moles"]
+         self.gram_amount = round(
+            operator.mul(self.mole_amount, self.mass), 4)
+         self.molecules = round(
+            operator.mul(self.mole_amount, AVOGADRO), 4)
+
+      if "grams" in kwargs:
+         self.gram_amount = kwargs["grams"]
+         self.mole_amount = round(
+            operator.__truediv__(self.gram_amount, self.mass), 4)
+         self.molecules = round(
+            operator.mul(self.mole_amount, AVOGADRO), 4)
+
+      if "molecules" in kwargs:
+         self.molecules = kwargs["molecules"]
+         self.mole_amount = round(
+            operator.__truediv__(self.molecules, AVOGADRO), 4)
+         self.gram_amount = round(
+            operator.mul(self.mass, self.mole_amount))
+
+      if "percent" in kwargs:
+         # Primarily if you are setting up elements for Compound.from_formula.
+         if float(kwargs["percent"]) >= 1:
+            raise TypeError("That is not a valid input for the percent "
+                            "field. Enter a decimal percent value.")
+         self.percent_of = kwargs["percent"]
+         self.mole_amount = False
+         self.gram_amount = False
+
+      if all(x in ["moles", "grams", "kwargs"] for x in [kwargs]):
+         raise ValueError("You cannot provide multiple quantities "
+                          "of the element at a single time.")
 
    @classmethod
    def from_molar_mass(cls, mass):
-      """Creates an Element class from a provided molar mass."""
+      """Instantiates an element from a provided molar mass.
+
+      Given a certain molar mass value, this method checks to see whether there
+      is an existing element with a defined molar mass value within 0.05 of the
+      provided molar mass value, and instantiates the class from that.
+
+      Examples
+      --------
+      Create a Boron atom from within 0.02 of its actual molar mass.
+
+      >>> boron = Element.from_molar_mass(10.8)
+
+      Parameters
+      ----------
+      mass: float
+         The molar mass of the element which you want to create.
+
+      Returns
+      -------
+      An instantiated Element class from the molar mass value.
+      """
       table = PeriodicTable()
 
       for indx, molar_mass in enumerate(table['AtomicMass']):
-         if abs(mass - molar_mass) <= 0.05: # If the molar mass provided is close to the actual one.
+         if abs(mass - molar_mass) <= 0.05:
+            # If the molar mass provided is close to the actual one.
             return cls(table['Symbol'][indx])
-      raise ValueError(f"Received invalid molar mass {mass}, not close to any molar masses on the periodic table.")
+      raise ValueError(f"Received invalid molar mass {mass}, not "
+                       f"close to any molar masses on the periodic table.")
 
    @classmethod
    def from_electron_configuration(cls, config):
-      """Creates an Element class from a provided electron configuration."""
+      """Instantiates an element from a provided electron configuration.
+
+      Given an electron configuration, either the complete one or the noble
+      gas abbreviation, this method gets the element associated with that
+      electron configuration and instantiates the Element class from it.
+
+      Examples
+      --------
+      Create a Magnesium atom from its noble gas electron configuration.
+
+      >>> magnesium = Element.from_electron_configuration('[Ne]3s2')
+
+      Parameters
+      ---------
+      config: str
+         The complete or noble gas abbreviated electron configuration.
+
+      Returns
+      -------
+      An instantiated Element class from the electron configruation.
+      """
       table = PeriodicTable()
 
       for indx, configuration in enumerate(table['ElectronConfiguration']):
@@ -125,7 +224,7 @@ class Element(object):
             raise ae
 
    def _get_full_electron_configuration(self):
-      """Returns the entire electron configuration of the element, ignoring the noble gas abbreviation."""
+      """Returns the entire electron configuration of the element."""
       config = ""
       if self.electron_configuration[0] == "[":
          config = Element(self.electron_configuration[1:3]).full_electron_configuration + " "
@@ -134,10 +233,12 @@ class Element(object):
       else:
          return self.electron_configuration
 
+   @ChemsolveDeprecationWarning('Element.calculate_moles', future_version = '2.0.0')
    def calculate_moles(self):
       """Calculates the class mole quantity from grams."""
       return round(operator.__truediv__(self.gram_amount, self.mass), 3)
 
+   @ChemsolveDeprecationWarning('Element.calculate_grams', future_version = '2.0.0')
    def calculate_grams(self):
       """Calculates the class gram quantity from moles."""
       return operator.mul(self.mole_amount, self.mass)
