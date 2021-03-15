f606932cca359bdf68f37989299819ab3246363d
amogh7joshi <joshi.amoghn@gmail.com>
2021-03-15 18:26:51 -0400

    Changed workings of invalid chemistry argument and added invalid compound error

--- chemsolve/utils/errors.py
+++ chemsolve/utils/errors.py
@@ -6,28 +6,53 @@ class ChemsolveError(Exception):
    """Base class for all Chemsolve errors."""
    pass
 
-class InvalidElementError(ChemsolveError, ValueError):
-   """Primarily invalid element error.
-
-   Raised when an invalid element or element property is received.
-   """
-   def __init__(self, element_or_property, property_type = None):
-      # Determine whether an element or property was passed.
-      self.element_or_property = element_or_property
+class InvalidChemistryArgumentError(ChemsolveError, ValueError):
+   """Base class for all invalid argument errors."""
+   def __init__(self, object_or_property,
+                property_type = None, chemical_type = None):
+      # Determine whether an object or property was passed.
+      self.object_or_property = object_or_property
       self.property_type = property_type
+
+      # Set the chemical type for the displayed output.
+      self.chemical_type = chemical_type
+      self.plurals = "s" if self.chemical_type in ['Element'] else ""
 
    def __str__(self):
       # Format the output as necessary.
       raised_str = "Received an invalid "
       if self.property_type == "type":
-         raised_str += "object of type {0}, expected an element.".format(
-            type(self.element_or_property))
-      elif isinstance(self.element_or_property, Element):
-         raised_str += "element name: {0}.".format(self.element_or_property)
+         raised_str += "object of type {0}, expected a{1} {2}.".format(
+            type(self.object_or_property), self.plurals, self.chemical_type)
+      elif isinstance(self.object_or_property, Element):
+         raised_str += "{0} name: {1}.".format(
+            self.chemical_type, self.object_or_property)
       else:
-         raised_str += "element property for {0}: {1}.".format(
-            self.element_or_property, self.property_type)
+         raised_str += "{0} property for {1}: {2}.".format(
+            self.chemical_type, self.object_or_property, self.property_type)
       return raised_str
 
    def __reduce__(self):
-      return self.__class__, self.element_or_property, self.property_type
+      return self.__class__, self.object_or_property, self.property_type
+
+class InvalidElementError(InvalidChemistryArgumentError, ValueError):
+   """Primary invalid element error.
+
+   Raised when an invalid element or element property is received.
+   """
+   def __init__(self, object_or_property,
+                property_type = None, chemical_type = "element"):
+      super(InvalidElementError, self).__init__(
+         object_or_property, property_type, chemical_type)
+
+
+class InvalidCompoundError(InvalidChemistryArgumentError):
+   """Primary invalid compound error.
+
+   Raised when an invalid compound or compound property is received.
+   """
+   def __init__(self, object_or_property,
+                property_type = None, chemical_type = "compound"):
+      super(InvalidCompoundError, self).__init__(
+         object_or_property, property_type, chemical_type)
+
