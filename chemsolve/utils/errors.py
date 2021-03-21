48aec4e54242231e854293285bc1a5c9ce3f3e89
amogh7joshi <joshi.amoghn@gmail.com>
2021-03-21 17:42:02 -0400

    Fix periodic table to raise an element error if an unknown name is received

--- chemsolve/utils/errors.py
+++ chemsolve/utils/errors.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding = utf-8 -*-
-from chemsolve.element import Element
 
 class ChemsolveError(Exception):
    """Base class for all Chemsolve errors."""
--- chemsolve/utils/periodictable.py
+++ chemsolve/utils/periodictable.py
@@ -1,18 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding = utf-8 -*-
 import os
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-directory, filename = os.path.split(__file__)
-PATH = os.path.join(directory, "assets", "PT_complete.csv")
+from chemsolve.utils.errors import InvalidElementError
+
+# Create the path to the periodic table.
+pt_path = os.path.join(os.path.dirname(__file__), "assets", "PT_complete.csv")
 
 class PeriodicTable(pd.DataFrame):
+   """A DataFrame containing the periodic table, and information about elements."""
    def __init__(self):
-      super().__init__(pd.read_csv(PATH))
+      super().__init__(pd.read_csv(pt_path))
       pd.set_option("display.max_rows", None, "display.max_columns", None)
 
    def get_properties(self, symbol):
-      properties = np.array(self.iloc[[self.index[self['Symbol'] == symbol].tolist()[0]]]).ravel()
+      """Get the different properties of a specific element."""
+      try:
+         properties = np.array(
+            self.iloc[[self.index[self['Symbol'] == symbol].tolist()[0]]]).ravel()
+      except IndexError:
+         raise InvalidElementError(symbol)
       elements = list(self)
       return dict(zip(elements, properties))
 
