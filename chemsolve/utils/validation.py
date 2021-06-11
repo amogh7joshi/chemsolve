98773b4ada292a4660e8bae474281145d607a46f
amogh7joshi <joshi.amoghn@gmail.com>
2021-06-10 23:21:02 -0400

    Added new validation method for constants/numbers

--- chemsolve/utils/validation.py
+++ chemsolve/utils/validation.py
@@ -4,6 +4,7 @@ from chemsolve.element import Element
 from chemsolve.compound import Compound
 from chemsolve.utils.periodictable import PeriodicTable
 from chemsolve.utils.errors import InvalidElementError
+from chemsolve.utils import constants
 
 def is_valid_element(element):
    """Checks whether a provided object is a valid element."""
@@ -13,7 +14,6 @@ def is_valid_element(element):
 
    # Otherwise, check whether it is in the periodic table.
    return element in [item for item in PeriodicTable()['Symbol']]
-
 
 def maybe_elements(*elements):
    """Validation method to check whether provided arguments are
@@ -38,3 +38,29 @@ def maybe_elements(*elements):
 
    # Return the converted elements.
    return converted_elements
+
+def resolve_float_or_constant(input_value, accept_none = True):
+   """Resolves an input as either a float/int object,
+   or a default chemistry-related constant."""
+   if input_value is None:
+      if accept_none:
+         return None
+      else:
+         raise ValueError(
+            f"Received invalid value {type(input_value)}."
+         )
+   if isinstance(input_value, (float, int)):
+      return input_value
+   elif isinstance(input_value, str):
+      try:
+         return getattr(constants, input_value)
+      except AttributeError:
+         raise AttributeError(
+            f"Received invalid constant value {input_value}."
+         )
+   else:
+      raise TypeError(
+         "Received invalid float input, "
+         "expected either a constant or a number."
+      )
+
